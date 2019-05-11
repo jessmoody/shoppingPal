2677e97db4eb955c3c65f500dd79f6f21e09857f
jessmoody <49352744+jessmoody@users.noreply.github.com>
2019-05-11 14:13:51 -0700

    list.html

--- shoppingPal.py
+++ shoppingPal.py
@@ -8,7 +8,16 @@ def home():
 @app.route("/inventory", methods=["POST"])
 def inventory():
     return render_template('inventory.html')
+ 
 
-@app.route("/list", methods=["POST"])
-def shoppingList():
+@app.route("/lists/tasklist/tasks", methods=["POST"]) #Add task
+def insert():
+    return render_template('list.html')
+
+@app.route("/lists/tasklist/tasks/task", methods=["PUT"]) #Update task
+def update():
+    return render_template('list.html')
+
+@app.route("/lists/tasklist/tasks/task", methods=["DELETE"])
+def delete():
     return render_template('list.html')
