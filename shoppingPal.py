119bb62edd47a7c5a60989290a2d7e8a60ba1577
jessmoody <49352744+jessmoody@users.noreply.github.com>
2019-05-11 18:58:44 -0700

    changed block comments to line comments, moved post method

--- shoppingPal.py
+++ shoppingPal.py
@@ -1,33 +1,27 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
+@app.route("/", method = "POST")
 def home():
-    firstItem = request.form['item1']
-    # fh = open(“inventoryFile.txt”, “a”) 
-    # fh.write(firstItem) 
-    # fh.close 
-    # Fh = open(“hello.txt”, “r”) 
-    # print fh.read() 
-    # fh.close()
-    return render_template("frontend.html", item1 = firstItem)
+    first_item = request.form['item1']
+    return render_template("frontend.html", item1 = first_item)
 
-""" @app.route("/inventory", methods=["POST"])
-def inventory():
-    item = request.form['firstItem']
-    return render_template('submitPage.html', firstItem = item)
+# @app.route("/inventory", methods=["POST"])
+# def inventory():
+#     item = request.form['firstItem']
+#     return render_template('submitPage.html', firstItem = item)
 
-@app.route("/lists/tasklist/tasks", methods=["POST"]) #Add task
-def insert():
-    return render_template('list.html')
+# @app.route("/lists/tasklist/tasks", methods=["POST"]) #Add task
+# def insert():
+#     return render_template('list.html')
 
-@app.route("/lists/tasklist/tasks/task", methods=["PUT"]) #Update task
-def update():
-    return render_template('list.html')
+# @app.route("/lists/tasklist/tasks/task", methods=["PUT"]) #Update task
+# def update():
+#     return render_template('list.html')
 
-@app.route("/lists/tasklist/tasks/task", methods=["DELETE"])
-def delete():
-    return render_template('list.html') """
+# @app.route("/lists/tasklist/tasks/task", methods=["DELETE"])
+# def delete():
+#     return render_template('list.html')
 
 
 if __name__ == '__main__':
